18011cfc345a41caedee6be1b46ce6c159919f18
fxDopa <131190297+rdopaa@users.noreply.github.com>
2023-08-13 14:57:09 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'fxDopa'
+
+description 'Command Stream for Users'
+
+server_scripts {
+    "main.lua",
+    "config.lua"
+}
+lua54 'yes'
---
+++ server.lua
@@ -0,0 +1,49 @@
+ESX = exports['es_extended']:getSharedObject()
+
+local isDeadly = false
+local deathCoords = {}
+
+RegisterServerEvent('esx:onPlayerDeath')
+AddEventHandler('esx:onPlayerDeath', function(data)
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if xPlayer then
+        deathCoords[source] = xPlayer.getCoords(true)
+        isDeadly = true
+        --print(deathCoords[source])
+    end
+end)
+
+RegisterServerEvent('playerSpawned')
+AddEventHandler('playerSpawned', function(spawn)
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if xPlayer then
+        deathCoords[source] = nil
+        isDeadly = false
+    end
+end)
+
+AddEventHandler('playerDropped', function(reason)
+    local source = source
+    local rawInventory = exports.ox_inventory:Inventory(source).items
+    local inventory = {}
+
+    if deathCoords[source] and isDeadly then
+        for _,v in pairs(rawInventory) do
+            inventory[#inventory + 1] = {
+                v.name,
+                v.count,
+                v.metadata
+            }
+        end
+
+        if #inventory > 0 then
+            exports.ox_inventory:CustomDrop('Loot Drop', inventory, deathCoords[source])
+        end
+        --print("Created Drop Loot"..deathCoords[source])
+        deathCoords[source] = nil
+    end
+end)
